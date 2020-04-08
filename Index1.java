fcbaba0f198c37f83fd6159ea1177b148ffceda7
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-04-08 15:58:52 +0530

    Create Index1.java

---
+++ Index1.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Index1 {
+	public static void main(String[] args) {
+		//create a driver object
+		//this is specific to each and every browser
+		// we will strictly implement the method of web driver
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Documents\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		//invoke chrome browser on the macj=hine
+		//invoke .exe file first
+		
+		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+		
+		
+	}
+}
